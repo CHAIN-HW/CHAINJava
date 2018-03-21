59c6b6895ee7c932a82b2912d3d9939b71ba3bd1
Duncan Cameron <dac31@hw.ac.uk>
2018-03-21 18:18:35 +0000

    rename test package

---
+++ src/test/java/chain/sql/SQLAdapterTest.java
@@ -0,0 +1,24 @@
+package chain.sql;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class SQLAdapterTest {
+
+    @Test
+    public void getConnection() throws SQLException {
+//        SQLAdapter adapter = new SQLAdapter("jdbc:mysql://mysql-server-1@macs.hw.ac.uk/dac31", "dac31", "sql");
+//        assertNotNull(adapter.getConnection());
+    }
+
+    @Test
+    public void getSchemaOntology() {
+    }
+
+    @Test
+    public void executeQuery() {
+    }
+}
