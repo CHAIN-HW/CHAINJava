8148cab3d2c84da789a0a85a3d4821dfa26cff7a
lewis785 <lewisfmcn@yahoo.co.uk>
2018-03-23 17:12:05 +0000

    Started working on storing database structure

---
+++ src/main/java/chain/sql/SQLQueryRepair.java
@@ -0,0 +1,32 @@
+package chain.sql;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.util.TablesNamesFinder;
+
+/**
+ * @author "Lewis McNeill"
+ *
+ */
+public class SQLQueryRepair {
+
+    private String query;
+
+    /**
+     *
+     */
+    private void repairQuery()
+    {
+
+    }
+
+    /**
+     * Gets repaired query
+     * @return
+     */
+    public String getRepairedQuery()
+    {
+        return query;
+    }
+}
---
+++ src/main/java/chain/sql/SQLTable.java
@@ -0,0 +1,55 @@
+package chain.sql;
+
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * @author Lewis McNeill
+ *
+ * Represents an sql table
+ */
+public class SQLTable {
+
+    private String tableName;
+    private Set<String> columnNames;
+
+    /**
+     * Constructs an object for an individual table
+     * @param tableName Name of the table
+     * @param columnNames Set of columns stored in the table
+     */
+    public SQLTable(String tableName, Set<String> columnNames)
+    {
+        this.tableName = tableName;
+        this.columnNames = columnNames;
+    }
+
+    /**
+     * Retrieves the name of the table
+     * @return The table name as string
+     */
+    public String getTableName()
+    {
+        return this.tableName;
+    }
+
+    /**
+     * Retrieves all the columns in the table
+     * @return Set containing all column names
+     */
+    public Set<String> getColumnNames()
+    {
+        return this.columnNames;
+    }
+
+    /**
+     * Checks if column exists in table
+     * @param column Name of column being looked for
+     * @return True if column exists false otherwise
+     */
+    public boolean containsColumn(String column)
+    {
+        return columnNames.contains(column);
+    }
+}
