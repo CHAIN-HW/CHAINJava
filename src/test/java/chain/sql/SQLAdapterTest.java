3aafacb394c64a7abe7dc2b249a8d4f34e252892
lewis785 <lewisfmcn@yahoo.co.uk>
2018-03-26 18:38:44 +0100

    Changed unit tests to use Mockito to mock db connection

--- src/test/java/chain/sql/SQLAdapterTest.java
+++ src/test/java/chain/sql/SQLAdapterTest.java
@@ -1,5 +1,6 @@
 package chain.sql;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -8,6 +9,7 @@ import static org.junit.Assert.*;
 
 public class SQLAdapterTest {
 
+    @Ignore("Forming connection will not work on travis - not really testing anything else")
     @Test
     public void getConnection() throws SQLException {
         SQLAdapter adapter = new SQLAdapter("jdbc:mysql://mysql-server-1.macs.hw.ac.uk/dac31", "dac31", "sql");
--- src/test/java/chain/sql/SQLQueryRunnerTest.java
+++ src/test/java/chain/sql/SQLQueryRunnerTest.java
@@ -3,21 +3,41 @@ package chain.sql;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SQLQueryRunnerTest {
 
     private Connection conn;
 
     @Before
     public void setup() throws SQLException {
-        SQLAdapter sql = new SQLAdapter("jdbc:mysql://mysql-server-1.macs.hw.ac.uk/dac31", "dac31", "sql");
-        conn = sql.getConnection();
+        SQLMockDatabase mockDb = new SQLMockDatabase();
+        conn = mockDb.getConnection();
+
+        ResultSet rsUsers = mock(ResultSet.class);
+        ResultSet rsUsers2 = mock(ResultSet.class);
+
+        mockDb.returnWhenQuerying("SELECT * FROM users", rsUsers, rsUsers2);
+
+        when(rsUsers.next()).thenReturn(true);
+        when(rsUsers.getInt("id")).thenReturn(1);
+        when(rsUsers.getString("firstname")).thenReturn("john");
+        when(rsUsers.getString("lastname")).thenReturn("smith");
+
+        when(rsUsers2.next()).thenReturn(true);
+        when(rsUsers2.getInt("id")).thenReturn(2);
+        when(rsUsers2.getString("firstname")).thenReturn("steve");
+        when(rsUsers2.getString("lastname")).thenReturn("johnson");
     }
 
     @After
@@ -30,7 +50,9 @@ public class SQLQueryRunnerTest {
         String query = "SELECT * FROM users";
         SQLQueryRunner queryRunner = new SQLQueryRunner(query, conn);
         ResultSet resultSet = queryRunner.getResults();
-        testResults(resultSet);
+        testResults(resultSet, 1, "john", "smith");
+        ResultSet resultSet2 = queryRunner.getResults();
+        testResults(resultSet2, 1, "john", "smith");
     }
 
     @Test
@@ -38,20 +60,23 @@ public class SQLQueryRunnerTest {
         String query = "SELECT * FROM users";
         SQLQueryRunner queryRunner = new SQLQueryRunner(query, conn);
         ResultSet resultSet = queryRunner.runQuery();
-        testResults(resultSet);
+        testResults(resultSet, 1, "john", "smith");
+        ResultSet resultSet2 = queryRunner.runQuery();
+        testResults(resultSet2, 2, "steve", "johnson");
     }
 
-    private void testResults(ResultSet resultSet) throws SQLException {
+    private void testResults(ResultSet resultSet, int expectedId, String expectedFirst, String expectedSecond) throws SQLException {
         resultSet.next();
 
         int id  = resultSet.getInt("id");
         String first = resultSet.getString("firstname");
         String last = resultSet.getString("lastname");
 
-        assertEquals("Result set id does not match on query run", 1, id);
-        assertEquals("Result set firstname does not match on query run", "lewis", first);
-        assertEquals("Result set lastname does not match on query run", "mcneill", last);
+        assertEquals("Result set id does not match on query run", expectedId, id);
+        assertEquals("Result set firstname does not match on query run", expectedFirst, first);
+        assertEquals("Result set lastname does not match on query run", expectedSecond, last);
 
         resultSet.close();
     }
+
 }
