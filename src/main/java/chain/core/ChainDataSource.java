434e436387a94d5576701e36095bdb813aed218c
Duncan Cameron <dac31@hw.ac.uk>
2018-03-27 13:24:44 +0100

    Updated adapter iwith methodology for repairing queries

--- src/main/java/chain/core/ChainDataSource.java
+++ src/main/java/chain/core/ChainDataSource.java
@@ -1,9 +1,11 @@
 package chain.core;
 
+import chain.sql.ChainDataSourceException;
+
 public interface ChainDataSource {
 
     public void getSchemaOntology();
 
-    public ChainResultSet executeQuery(String query);
+    public ChainResultSet executeQuery(String query) throws ChainDataSourceException;
 
 }
--- src/test/java/chain/sql/SQLAdapterTest.java
+++ src/test/java/chain/sql/SQLAdapterTest.java
@@ -1,6 +1,7 @@
 package chain.sql;
 
 import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -9,10 +10,17 @@ import static org.junit.Assert.*;
 
 public class SQLAdapterTest {
 
+
+    private SQLAdapter adapter;
+
+    @Before
+    public void setup() throws ChainDataSourceException {
+        adapter = new SQLAdapter("jdbc:mysql://mysql-server-1.macs.hw.ac.uk/dac31", "dac31", "sql");
+    }
+
     @Ignore("Forming connection will not work on travis - not really testing anything else")
     @Test
-    public void getConnection() throws SQLException {
-        SQLAdapter adapter = new SQLAdapter("jdbc:mysql://mysql-server-1.macs.hw.ac.uk/dac31", "dac31", "sql");
+    public void getConnection() {
         assertNotNull(adapter.getConnection());
     }
 
@@ -22,5 +30,6 @@ public class SQLAdapterTest {
 
     @Test
     public void executeQuery() {
+
     }
 }
