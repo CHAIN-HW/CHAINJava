ae392309f580f91d1271789bab3fb8a6bef28db0
Duncan Cameron <dac31@hw.ac.uk>
2018-03-27 13:24:44 +0100

    Updated adapter iwith methodology for repairing queries

---
+++ src/main/java/chain/sql/SQLResultSet.java
@@ -0,0 +1,10 @@
+package chain.sql;
+
+import chain.core.ChainResultSet;
+
+import java.sql.ResultSet;
+
+public class SQLResultSet implements ChainResultSet {
+    public SQLResultSet(ResultSet results) {
+    }
+}
