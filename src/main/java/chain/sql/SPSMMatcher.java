1992923ccef2abc5b584e1cad5a3ede525e63696
Duncan Cameron <dac31@hw.ac.uk>
2018-03-29 13:18:08 +0100

    setup matching framework

---
+++ src/main/java/chain/sql/SPSMMatcher.java
@@ -0,0 +1,12 @@
+package chain.sql;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Set;
+
+public class SPSMMatcher {
+
+    public String match(String tableName, Set<String> tableNames) {
+        throw new NotImplementedException();
+    }
+}
