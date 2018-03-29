0533119f29c90ea2d2b7bf9fc3ad2d4a985a34a6
lewis785 <lewisfmcn@yahoo.co.uk>
2018-03-29 19:10:54 +0100

    Commenting

--- src/main/java/chain/sql/SQLQueryRepair.java
+++ src/main/java/chain/sql/SQLQueryRepair.java
@@ -3,6 +3,7 @@ package chain.sql;
 import chain.sql.visitors.SQLSelectTableVisitor;
 import it.unitn.disi.smatch.SMatchException;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.util.SelectUtils;
 import org.slf4j.impl.StaticMarkerBinder;
 
 import java.sql.Connection;
@@ -12,8 +13,9 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author "Lewis McNeill"
+ * SQLQueryRepair
  *
+ * Repair a repair process on the defined query
  */
 public class SQLQueryRepair {
 
@@ -21,18 +23,35 @@ public class SQLQueryRepair {
     private SQLDatabase db;
     private Statement stmt;
 
-
+    /**
+     * Constructor for SQLQueryRepair
+     * @param query is the SQL query that being repaired
+     * @param db is the structure of the database being queried
+     * @throws ChainDataSourceException
+     * @throws SQLException
+     */
     public SQLQueryRepair(String query, SQLDatabase db) throws ChainDataSourceException, SQLException {
         this.analyser = new SQLQueryAnalyser(query);
         this.stmt = analyser.getStatement();
         this.db = db;
     }
 
+    /**
+     * Runs the repair process and returns the repaired query
+     * @return repaired query as a string
+     * @throws SPSMMatchingException
+     * @throws SMatchException
+     */
     public String runRepairer() throws SPSMMatchingException, SMatchException {
         repairTables();
         return this.analyser.toSQL();
     }
 
+    /**
+     * Repairs table names present in the query
+     * @throws SPSMMatchingException
+     * @throws SMatchException
+     */
     private void repairTables() throws SPSMMatchingException, SMatchException {
         List<String> tables =  this.analyser.getTables();
         SQLNameMatcherManager manager = new SQLNameMatcherManager(tables, this.db);
@@ -43,14 +62,26 @@ public class SQLQueryRepair {
 
     }
 
+    /**
+     * Repairs column names present in the query
+     */
     private void repairColumns()
     {
 
     }
 
+    /**
+     * Replaces table names
+     * @param name what the table should be renamed
+     */
     private void repairTableName(String name) {
         SQLSelectTableVisitor visitor = new SQLSelectTableVisitor(name);
         stmt.accept(visitor);
+    }
+
+    private void repairColumnName()
+    {
+
     }
 
 
